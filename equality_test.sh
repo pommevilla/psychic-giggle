086059a9c136dea0ab378c6d8502c04461b7c4ea
pommevilla <pvillanueva13@gmail.com>
2020-01-17 14:16:30 -0600

    Add a simple file contents comparison

--- equality_test.sh
+++ equality_test.sh
@@ -4,4 +4,17 @@ testEquality() {
 	assertEquals 1 1
 }
 
+oneTimeSetUp(){
+    echo "Something dumb" > temp_file
+}
+
+testFileContents(){
+    CONTENTS=$(cat temp_file)
+    assertEquals "Contents of temp_file and thing didn't match" "$CONTENTS" "Something dumb"
+}
+
+oneTimeTearDown(){
+    rm temp_file
+}
+
 . shunit2-2.1.7/shunit2
